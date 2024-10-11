b69c9d417a5a3d86bde78a0804ff02ccf27dacb0
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-10-11 05:36:43 +0530

    basics of python

---
+++ firstProgram.py
@@ -0,0 +1,44 @@
+# print("Hello World")
+
+# print(22)
+
+# print( 22 + 1 )
+
+# age = 22
+
+# print("Age",age)
+
+# print("Current Age",age + 1)
+
+# name = "Niranjan Raju"
+
+# age = 22
+
+# place ="Guntur, A.P"
+
+# print("Hi, I am ",name,"from ",place, '.i am ',age," years old")
+
+# name = input("enter your name: ")
+
+# age = int(input("enter your age: "))
+
+# print("My Name is",name,"i am",age,"years old!")
+
+
+# a = int(input("enter a value:"))
+# b = int(input("enter b value:"))
+
+# print("sum of a and b:",a + b)
+
+# side = float(input("enter side of a square:"))
+
+# print("area of a square:",side * side)
+
+
+# a = float(input("enter a value:"))
+# b = float(input("enter b value:"))
+
+# avg = (a + b)/2
+
+# print("avg of a and b",avg)
+
